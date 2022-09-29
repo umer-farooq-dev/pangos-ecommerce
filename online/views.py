b33b837ad0c8e8f2f9edf5d65e3a3f573e0d27fa
umer-farooq-dev <ufkeyfans786@gmail.com>
2022-09-29 23:55:08 +0500

    Updated

--- online/views.py
+++ online/views.py
@@ -26,16 +26,6 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
-
-
-
-def index(request):
-    return render(request, 'index.html')
-
-
-def about(request):
-    return render(request, 'about.html')
-    
     
 
 
